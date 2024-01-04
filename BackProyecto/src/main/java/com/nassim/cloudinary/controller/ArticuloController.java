f134989ad026ec08d8f8e302c96be5f8e1689242
Jonathan Mostacero <jsmithmostacero@gmail.com>
2024-01-04 11:24:33 -0500

    Prueba back

--- BackProyecto/src/main/java/com/nassim/cloudinary/controller/ArticuloController.java
+++ BackProyecto/src/main/java/com/nassim/cloudinary/controller/ArticuloController.java
@@ -23,6 +23,7 @@ import java.util.Optional;
 @RequestMapping("/articulo")
 @CrossOrigin(origins = "http://localhost:4200")
 public class ArticuloController {
+    // hola
     @Autowired
     CloudinaryService cloudinaryService;
 
